5ae343a5c76a3c09f41840e2442d4849746c81fd
Daniel Cipriano <61021523+dancipri@users.noreply.github.com>
2020-05-16 00:22:26 -0300

    upload

---
+++ Exc3/src/Exercicio3/ex3.java
@@ -0,0 +1,22 @@
+package Exercicio3;
+
+public class ex3 {
+	public static void main (String[] args)
+	{
+		int x;
+		for (x=229;x<=454;x++)
+		{
+			if(x>298 && x<=398)
+			{
+				x=x+2;
+				System.out.println((x));
+			}
+			else
+			{
+				x=x+4;
+				System.out.println((x));		
+			}
+		}
+	}
+
+}
